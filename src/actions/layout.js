18086e3265cb86fd232af36f80be7831692f6f83
Sylvain Saint-André <sylvain.saint-andre@laposte.net>
2018-09-29 13:52:48 +0200

    🐞 Remove deprecated redux router system
    Not replaced for now because not used yet

--- src/actions/layout.js
+++ src/actions/layout.js
@@ -1,4 +1,3 @@
-import { push } from 'react-router-redux'
 
 export const toggleLeftDrawer = () => (dispatch) => {
     dispatch({ type: 'TOGGLE_LEFT_DRAWER', payload: null })    
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux'
 import layout from './layout'
-import { routerReducer } from 'react-router-redux'
 
 export default combineReducers({
 	layout,
-	router: routerReducer
 })
