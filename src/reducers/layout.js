e9292649dc6175ea7314df76948e10fbf49bbb61
Sylvain Saint-André <sylvain.saint-andre@laposte.net>
2018-10-09 15:24:43 +0200

    🔨📐#WIP Refactor layout reducer (SDA-2)

--- src/reducers/layout.js
+++ src/reducers/layout.js
@@ -1,3 +1,7 @@
+/*****************/
+/* Reducer shape */
+/*****************/
+
 const initialState = {
     leftDrawer: {
         open: false
@@ -15,7 +19,11 @@ const initialState = {
     }
 }
 
-const addSnackbarMessage = (message, snackbarState) => {
+/*********************/
+/* Utility functions */
+/*********************/
+
+const handleAddSnackbarMessage = (snackbarState, message) => {
     let newSnackbarState = {
         ...snackbarState,
         queue: [
@@ -31,11 +39,11 @@ const addSnackbarMessage = (message, snackbarState) => {
             open: false
         } 
     } else {
-        return processSnackbarQueue(newSnackbarState);
+        return handleProcessSnackbarQueue(newSnackbarState);
     }
 }
 
-const processSnackbarQueue = snackbarState => {
+const handleProcessSnackbarQueue = snackbarState => {
     if (snackbarState.queue.length > 0) {
         return {
             ...snackbarState,
@@ -48,90 +56,102 @@ const processSnackbarQueue = snackbarState => {
     return snackbarState
 }
 
-const handleSnackbarClose = (reason, snackbarState) => {
-    if (reason === 'clickaway') 
-        return snackbarState 
-
-    return {
-        ...snackbarState,
-        open: false
-    }    
+const handleSnackbarClose = (snackbarState, action) => {
+    if (action.payload === 'clickaway') return snackbarState 
+    return handleOpenClose(snackbarState, false)
 }
+
+const handleOpenClose = (storeObject, value) => ({ ...storeObject, open: value }) 
+
+/**************************/
+/* Case reducer functions */
+/**************************/
+
+const newDiscussionLaunch = (state, action) => ({ ...state, 
+    newDiscussionForm: handleOpenClose(state.newDiscussionForm, true) 
+})
+
+const newDiscussionSaveSuccess = (state, action) => ({ ...state, 
+    newDiscussionForm: handleOpenClose(state.newDiscussionForm, false),
+    snackbar: handleAddSnackbarMessage(state.snackbar, action.payload) 
+})
+
+const newDiscussionCancel = (state, action) => ({ ...state, 
+    newDiscussionForm: handleOpenClose(state.newDiscussionForm, false) 
+})
+
+const toggleLeftDrawer = (state, action) => ({ ...state, 
+    leftDrawer: handleOpenClose(state.leftDrawer, !state.leftDrawer.open) 
+})
+
+const closeLeftDrawer = (state, action) => ({ ...state, 
+    leftDrawer: handleOpenClose(state.leftDrawer, false) 
+})
+
+const openLeftDrawer = (state, action) => ({ ...state, 
+    leftDrawer: handleOpenClose(state.leftDrawer, true) 
+})
+
+const snackbarClose = (state, action) => ({ ...state, 
+    snackbar: handleSnackbarClose(state.snackbar, action) 
+})
+
+const snackbarExited = (state, action) => ({ ...state, 
+    snackbar: handleProcessSnackbarQueue(state.snackbar) 
+})
+
+const parseProfileError = (state, action) => ({ ...state, 
+    snackbar: handleAddSnackbarMessage(state.snackbar, action.payload) 
+})
+
+const requestProfileError = (state, action) => ({ ...state, 
+    snackbar: handleAddSnackbarMessage(state.snackbar, action.payload)         
+})
+
+const newDiscussionSaveError = (state, action) => ({ ...state, 
+    snackbar: handleAddSnackbarMessage(state.snackbar, action.payload) 
+})
+
+const explorerOpen = (state, action) => ({ ...state, 
+    explorer: handleOpenClose(state.explorer, true)     
+})
+
+const explorerClose = (state, action) => ({ ...state, 
+    explorer: handleOpenClose(state.explorer, false)     
+})
+
+/******************/
+/* Reducer switch */
+/******************/
 
 const layout = (state = initialState, action) => {
     switch (action.type) {
-        case 'NEW_DISCUSSION_LAUNCH':
-            return {
-                ...state,  
-                newDiscussionForm: {
-                    ...state.newDiscussionForm,
-                    open: true
-                }
-            }           
+        case 'NEW_DISCUSSION_LAUNCH': 
+            return newDiscussionLaunch(state, action)           
         case 'NEW_DISCUSSION_SAVE_SUCCESS':    
+            return newDiscussionSaveSuccess(state, action) 
         case 'NEW_DISCUSSION_CANCEL':
-            return {
-                ...state,  
-                newDiscussionForm: {
-                    ...state.newDiscussionForm,
-                    open: false
-                }
-            }           
+            return newDiscussionCancel(state, action)       
         case 'TOGGLE_LEFT_DRAWER':
-            return {
-                ...state, leftDrawer: {
-                    ...state.leftDrawer,
-                    open: !state.leftDrawer.open
-                }      
-            }    
+            return toggleLeftDrawer(state, action)       
         case 'CLOSE_LEFT_DRAWER':
-            return {
-                ...state, leftDrawer: {
-                    ...state.leftDrawer,
-                    open: false
-                }      
-            }    
+            return closeLeftDrawer(state, action)       
         case 'OPEN_LEFT_DRAWER':
-            return {
-                ...state, leftDrawer: {
-                    ...state.leftDrawer,
-                    open: true
-                }      
-            }    
-        case 'SNACKBAR_CLOSE': 
-            return {
-                ...state, 
-                snackbar: handleSnackbarClose(action.payload, state.snackbar)
-            }               
+            return openLeftDrawer(state, action)       
+        case 'SNACKBAR_CLOSE':
+            return snackbarClose(state, action)                   
         case 'SNACKBAR_EXITED': 
-            return {
-                ...state, 
-                snackbar: processSnackbarQueue(state.snackbar)
-            }               
+            return snackbarExited(state, action)                              
         case 'PARSE_PROFILE_ERROR':                    
+            return parseProfileError(state, action)                              
         case 'REQUEST_PROFILE_ERROR':    
+            return requestProfileError(state, action)                              
         case 'NEW_DISCUSSION_SAVE_ERROR':    
-        case 'NEW_DISCUSSION_SAVE_SUCCESS':    
-            return {
-                ...state, 
-                snackbar: addSnackbarMessage(action.payload, state.snackbar)
-            }      
+            return newDiscussionSaveError(state, action)                                  
         case 'EXPLORER_OPEN':
-            return {
-                ...state,
-                explorer: {
-                    ...state.explorer,
-                    open: true  
-                }   
-            }
+            return explorerOpen(state, action)
         case 'EXPLORER_CLOSE':
-            return {
-                ...state,
-                explorer: {
-                    ...state.explorer,
-                    open: false 
-                } 
-            }                                                                               
+            return explorerClose(state, action)                                             
         default:
             return state
     }
